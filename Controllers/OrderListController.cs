e634895953a59b834eb8e8f48127c1115da582c3
MappetProd <fwexample821@gmail.com>
2024-11-03 17:03:34 +0300

    order list view and controller added

---
+++ Controllers/OrderListController.cs
@@ -0,0 +1,19 @@
+﻿using LogisticApp.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogisticApp.Controllers
+{
+    public class OrderListController : Controller
+    {
+        private readonly LogisticAppContext _context;
+        public OrderListController(LogisticAppContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.Orders = _context.Orders.ToList();
+            return View("~/Views/OrderList.cshtml");
+        }
+    }
+}
